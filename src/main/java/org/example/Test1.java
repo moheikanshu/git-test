2005fb70e3ef9d1c4d937c6f0c0c047f23bd303a
Star <1043965275@qq.com>
2022-01-08 13:25:13 +0800

    hot-fix commit 1

--- src/main/java/org/example/Test1.java
+++ src/main/java/org/example/Test1.java
@@ -17,5 +17,6 @@ public class Test1 {
         System.out.println("git test3");
         System.out.println("git test3");
         System.out.println("git test3");
+        System.out.println("git test4");
     }
 }
